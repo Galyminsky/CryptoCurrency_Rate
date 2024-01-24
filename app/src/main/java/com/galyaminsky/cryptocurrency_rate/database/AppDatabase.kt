4c6ee57fc4d8626f3bbd12f00088c0cffe8d825f
Galyaminsky_Linux <jobforandroid@proton.me>
2024-01-24 11:51:19 +0600

    # AppDatabase

--- app/src/main/java/com/galyaminsky/cryptocurrency_rate/database/AppDatabase.kt
+++ app/src/main/java/com/galyaminsky/cryptocurrency_rate/database/AppDatabase.kt
@@ -4,19 +4,23 @@ import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
         private var db: AppDatabase? = null
         private const val DB_NAME = "main.db"
+        private val LOCK = Any()
 
         fun getInstance(context: Context): AppDatabase {
-            db?.let { return it }
-
-            val instance: AppDatabase =
-                Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
-            db = instance
-            return instance
+            synchronized(LOCK)
+            {
+                db?.let { return it }
+                val instance: AppDatabase =
+                    Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+                db = instance
+                return instance
+            }
         }
     }
 }
