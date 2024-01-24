779fb5845fac628f53f44e69ba3cfd61d2db5a66
Galyaminsky_Linux <jobforandroid@proton.me>
2024-01-24 11:48:24 +0600

    # AppDatabase create

---
+++ app/src/main/java/com/galyaminsky/cryptocurrency_rate/database/AppDatabase.kt
@@ -0,0 +1,22 @@
+package com.galyaminsky.cryptocurrency_rate.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        private var db: AppDatabase? = null
+        private const val DB_NAME = "main.db"
+
+        fun getInstance(context: Context): AppDatabase {
+            db?.let { return it }
+
+            val instance: AppDatabase =
+                Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+            db = instance
+            return instance
+        }
+    }
+}
