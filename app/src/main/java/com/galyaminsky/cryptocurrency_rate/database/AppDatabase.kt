77d36aa7f517780197d5c2037083c4f3ffbf2cd7
Galyaminsky_Linux <jobforandroid@proton.me>
2024-01-24 12:02:54 +0600

    # AppDatabase Completed

--- app/src/main/java/com/galyaminsky/cryptocurrency_rate/database/AppDatabase.kt
+++ app/src/main/java/com/galyaminsky/cryptocurrency_rate/database/AppDatabase.kt
@@ -1,10 +1,12 @@
 package com.galyaminsky.cryptocurrency_rate.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.galyaminsky.cryptocurrency_rate.pojo.CoinPriceInfo
 
-
+@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
@@ -23,4 +25,5 @@ abstract class AppDatabase : RoomDatabase() {
             }
         }
     }
+    abstract fun CoinPriceInfoDao(): CoinPriceInfoDao
 }
