5bebf922cbe7eb8721052b204a4451f35cd0f1b1
Galyaminsky_Linux <jobforandroid@proton.me>
2024-01-25 09:17:47 +0600

    # ViewModel_1q

---
+++ app/src/main/java/com/galyaminsky/cryptocurrency_rate/CoinViewModel.kt
@@ -0,0 +1,34 @@
+package com.galyaminsky.cryptocurrency_rate
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.galyaminsky.cryptocurrency_rate.api.ApiFactory
+import com.galyaminsky.cryptocurrency_rate.database.AppDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDatabase.getInstance(application)
+    private val compositeDisposable = CompositeDisposable()
+
+    val priceList = db.CoinPriceInfoDao().getPriceList()
+
+    fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("MyLog", it.toString())
+            }, {
+                Log.d("MyLog", it.message.toString())
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- app/src/main/java/com/galyaminsky/cryptocurrency_rate/MainActivity.kt
+++ app/src/main/java/com/galyaminsky/cryptocurrency_rate/MainActivity.kt
@@ -1,36 +1,22 @@
 package com.galyaminsky.cryptocurrency_rate
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.galyaminsky.cryptocurrency_rate.api.ApiFactory
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
+import androidx.lifecycle.ViewModelProvider
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val disposable = ApiFactory.apiService.getTopCoinsInfo()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("MyLog", it.toString())
-            }, {
-                Log.d("MyLog", it.message.toString())
-            })
-        compositeDisposable.add(disposable)
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
-    }
 }
 
 
