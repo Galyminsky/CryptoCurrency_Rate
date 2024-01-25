0ebb5d5cf72a4b890dea9679bbc7ac1ea2f72956
Galyaminsky_Linux <jobforandroid@proton.me>
2024-01-25 09:49:17 +0600

    # ViewModel getPriceListFromRawData

--- app/src/main/java/com/galyaminsky/cryptocurrency_rate/CoinViewModel.kt
+++ app/src/main/java/com/galyaminsky/cryptocurrency_rate/CoinViewModel.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.galyaminsky.cryptocurrency_rate.api.ApiFactory
 import com.galyaminsky.cryptocurrency_rate.database.AppDatabase
+import com.galyaminsky.cryptocurrency_rate.pojo.CoinPriceInfo
+import com.galyaminsky.cryptocurrency_rate.pojo.CoinPriceInfoRawData
+import com.google.gson.Gson
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -27,6 +30,26 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
             }, {
                 Log.d("text_myLog", it.message.toString())
             })
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getPriceListFromRawData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if (jsonObject == null) return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
     }
 
     override fun onCleared() {
